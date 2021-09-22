30fd9beae32c9cee85de1d5ef8338f8f011c7bb3
Tajeshwar <tkhara@gmail.com>
2021-09-22 14:11:57 +0530

    Lorem generator done.

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import data from './data'
+function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let amount = parseInt(count)
+    if (amount > 8) {
+      amount = 8
+    } else if (amount < 0) {
+      amount = 1
+    }
+    let partArray = data.slice(0, amount)
+    setText(partArray)
+  }
+
+  return (
+    <section className='section-center'>
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='text'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type='submit' className='btn'>
+          generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
+    </section>
+  )
+}
+
+export default App
